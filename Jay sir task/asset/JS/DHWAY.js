14a14921fc41ca2a9cbdf5560f118a26b4e99f22
Janvichavda <janvichavda2099@gmail.com>
2023-10-29 16:39:21 +0530

    DHWay

--- Jay sir task/asset/JS/DHWAY.js
+++ Jay sir task/asset/JS/DHWAY.js
@@ -35,7 +35,7 @@ var swiper = new Swiper(".mySwiper3", {
 var swiper = new Swiper(".mySwiper4", {
     slidesPerView: 1,
     spaceBetween: 10,
-    rewind: true,
+    // rewind: true,
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
@@ -45,7 +45,7 @@ var swiper = new Swiper(".mySwiper4", {
         prevEl: ".prev4",
     },
     pagination: {
-        el: ".swiper-pagination3",
+        el: ".swiper-pagination",
         clickable: true,
     },
     breakpoints: {
@@ -70,6 +70,7 @@ var swiper = new Swiper(".mySwiper2", {
     slidesPerView: 1,
     spaceBetween: 10,
     rewind: true,
+    loop:true,
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
@@ -88,7 +89,7 @@ var swiper = new Swiper(".mySwiper2", {
             spaceBetween: 20,
         },
         768: {
-            slidesPerView: 3,
+            slidesPerView: 2,
             spaceBetween: 40,
         },
         1024: {
